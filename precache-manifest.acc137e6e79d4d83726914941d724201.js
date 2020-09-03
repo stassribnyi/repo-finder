e5405bdc91b601d85b7003b16cbf918f0f4569cd
Stas Sribnyi <stas.sribnyi@gmail.com>
2020-09-03 21:34:10 +0300

    Updates

---
+++ precache-manifest.acc137e6e79d4d83726914941d724201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8186b27831d7377db0586982f5b5f56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18f8f3bbf29b7ad07d74",
+    "url": "/static/js/2.9a82917c.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.9a82917c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8a7538f0d5cb892387d",
+    "url": "/static/js/main.e735117a.chunk.js"
+  },
+  {
+    "revision": "01fed9c343cb9db1eb41",
+    "url": "/static/js/runtime-main.b42f7a3c.js"
+  }
+]);
