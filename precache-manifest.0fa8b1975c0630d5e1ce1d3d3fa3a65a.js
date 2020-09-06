f588856d67411ebe25d66dbbb3d8e3a9d03a9a8b
stassribnyi <stas.sribnyi@gmail.com>
2020-09-06 19:21:29 +0000

    Deploying to gh-pages from  @ fd1d3685bca628c3257da418bfa7b5a65aef0387 🚀

---
+++ precache-manifest.0fa8b1975c0630d5e1ce1d3d3fa3a65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d143f9f6367a7f8e0fc3ab378c6942",
+    "url": "https://stassribnyi.github.io/repo-finder/index.html"
+  },
+  {
+    "revision": "394e18064442f4e3dc79",
+    "url": "https://stassribnyi.github.io/repo-finder/static/js/2.5db2dc65.chunk.js"
+  },
+  {
+    "revision": "52fa51845369cf375a9e35a3fae38893",
+    "url": "https://stassribnyi.github.io/repo-finder/static/js/2.5db2dc65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "579c20c6187494c1bce5",
+    "url": "https://stassribnyi.github.io/repo-finder/static/js/main.be152871.chunk.js"
+  },
+  {
+    "revision": "0dc1031b41c64688f1c9",
+    "url": "https://stassribnyi.github.io/repo-finder/static/js/runtime-main.011d493e.js"
+  }
+]);
